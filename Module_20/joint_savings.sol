bfee1682be3ceb7c0923903c67ac52556633c774
marissagonzales468 <113739944+marissagonzales468@users.noreply.github.com>
2023-02-12 23:10:14 -0800

    Update joint_savings.sol

--- Module_20/joint_savings.sol
+++ Module_20/joint_savings.sol
@@ -92,7 +92,4 @@ contract JointSavings {
 }
 
     /*
-    Finally, add the **default fallback function** so that your contract can store Ether sent from outside the deposit function.
-    */
-    // YOUR CODE HERE!
-}
+ 
